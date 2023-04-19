bb4b198a4be8aa956d00ce21f9028b9875e37f2c
Andi Barron <andibarron7@gmail.com>
2023-04-19 15:58:19 -0700

    Slam motion working

--- Assets/scripts/Enemy Scripts/MirrorScripts/SlamAttack.cs
+++ Assets/scripts/Enemy Scripts/MirrorScripts/SlamAttack.cs
@@ -6,26 +6,40 @@ using TheKiwiCoder;
 [System.Serializable]
 public class SlamAttack : ActionNode
 {
-    public MirrorBossMirror posessedMirror;
-    public float fallSpeed = 1.0f;
-    public float rotationSpeed = 5.0f;
-    public float animationTime;
-    public bool animationIsPlaying = false;
+    private MirrorBossMirror posessedMirror;
 
+    private float animationTime;
+    private bool animationIsPlaying = false;
+    
     private Vector3 initialPosition;
     private Quaternion initialRotation;
-    private float fallForwardRotation;
+    private Quaternion rotationDirection;
+
+    Vector3 eulerRotation;
+    Quaternion newRotation;
+
+    private float yRotation;
+    private float rotationSpeed = 3.5f;
+
+    private float fallAnimationTime;
+    private float riseAnimationTime;
 
     protected override void OnStart() {
-        GameObject mirrorMain = GameObject.Find("BossRoot");
+        GameObject mirrorMain = GameObject.Find("BossRoot"); //gets mirror boss from scene
         if (mirrorMain == null) {
             Debug.LogError("Cannot find Boss");
         }
-        animationTime = 5;
+
+        posessedMirror = mirrorMain.GetComponent<MirrorBossMain>().currPosessedMirror; //gets current possesed mirror
+
+        initialPosition = posessedMirror.transform.position;
+        initialRotation = posessedMirror.transform.rotation; //gets initial rotation of posessed mirror
+        yRotation = posessedMirror.transform.rotation.eulerAngles.y;
+        newRotation = posessedMirror.transform.rotation;
+
+        fallAnimationTime = 5.0f; //temp animation time for mirror falling
+        riseAnimationTime = 5.0f;
         animationIsPlaying = true;
-        posessedMirror = mirrorMain.GetComponent<MirrorBossMain>().currPosessedMirror;
-        initialRotation = posessedMirror.transform.rotation;
-        fallForwardRotation = initialRotation.eulerAngles.z;;
     }
 
     protected override void OnStop() {
@@ -33,14 +47,24 @@ public class SlamAttack : ActionNode
 
     protected override State OnUpdate() {
         if(animationIsPlaying){
-            if(animationTime > 0){
-                animationTime -= Time.deltaTime;
-                fallForwardRotation -= 0.1f;
-                posessedMirror.transform.rotation = Quaternion.Euler(fallForwardRotation, 0.0f, 0.0f);
+            if(fallAnimationTime > 0){
+                fallAnimationTime -= Time.deltaTime * rotationSpeed * 2; //faster fall speed lead to faster animation
+                rotationDirection = Quaternion.Euler(0, yRotation, -90);
+                posessedMirror.transform.rotation = Quaternion.Lerp(initialRotation, rotationDirection, (1 - fallAnimationTime / 5)); //kinda got chat gpt to help me with this line
+                //work in progress for getting the position of the block to be positioned better as it falls
+                //posessedMirror.transform.position = Vector3.Lerp(initialPosition, posessedMirror.transform.position + posessedMirror.transform.forward * 5, (1 - animationTime / 5));
+                newRotation = posessedMirror.transform.rotation;
                 return State.Running;
             }
-            else{
-                animationTime = 0;
+            else if(riseAnimationTime > 0){
+                riseAnimationTime -= Time.deltaTime * rotationSpeed;
+                rotationDirection = Quaternion.Euler(0, yRotation, 0);
+                posessedMirror.transform.rotation = Quaternion.Lerp(newRotation, rotationDirection, (1 - riseAnimationTime / 5));
+                return State.Running;
+            }
+            else{ //returns success when animation is finished.
+                fallAnimationTime = 0;
+                riseAnimationTime = 0;
                 animationIsPlaying = false;
                 return State.Success;
             }
