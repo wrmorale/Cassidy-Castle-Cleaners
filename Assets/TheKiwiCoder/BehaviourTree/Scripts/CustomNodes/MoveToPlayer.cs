8a4b0d6bf5eca9a33ab3ab423c1e7f7d24a743c7
Santiago Ponce <saponce@ucsc.edu>
2023-04-22 13:09:21 -0700

    Comments on script templates

--- Assets/TheKiwiCoder/BehaviourTree/Scripts/CustomNodes/MoveToPlayer.cs
+++ Assets/TheKiwiCoder/BehaviourTree/Scripts/CustomNodes/MoveToPlayer.cs
@@ -16,8 +16,9 @@ public class MoveToPlayer : ActionNode
     }
 
     protected override State OnUpdate() {
+        //PlayerBody.position - position of Rigidbody of Enemy
         Vector3 toPlayer = context.enemy.playerBody.position - context.rigidbody.position;
-        //Maybe the context.player position does not update? But then it wouldn't make sense why the bug happens when I get near him
+
         toPlayer.y = 0; //Ignore player's vertical position
         Vector3 movement = toPlayer.normalized * context.enemy.movementSpeed * Time.fixedDeltaTime;
         context.rigidbody.MovePosition(context.rigidbody.position + (movement));
