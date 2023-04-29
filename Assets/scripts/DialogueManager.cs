ef737d522e0c2577c5e2972e57bd0dd4d9220fbc
Jessica <jhuan221@ucsc.edu>
2023-04-29 15:13:48 -0700

    editing the dialogue manager, fixing game object error

--- Assets/scripts/DialogueManager.cs
+++ Assets/scripts/DialogueManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
@@ -11,38 +12,64 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI dialogueText;
     private Queue<string> sentences;
 
+    public GameObject dialoguebox;
+
+    public static int dialogueIndex;
+
+    private Dictionary<string, string> dialogueDatabase = new Dictionary<string, string>()
+    {
+        {
+            "Cassidy", "Listen, new hire. I know you want to jump right into things, but we should go ahead and teach you the basics. First, clean these dust piles. Look around if you don't see them right in front of you.\r\n"
+        },
+        {
+            "The Maid", "I'm not that new to cleaning, you know ..."
+        },
+        {
+            "Cassidy", "Good work. Make sure you move to the next room, and quickly."
+        },
+        {
+            "Cassidy", "Oh, we didn't tell you but I hope you know how to fight, because this castle is full of monsters. Good luck."
+        },
+        {
+            "The Maid", "Talk about a hostile work environment."
+        },
+        {
+            "Cassidy", "Congradulation on not dying. Remember, you get paid to clean everything, not just the floors, so take out these monsters while you are at it."
+        }
+
+    };
+
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new Queue<string>();
+        dialogueIndex = 0;
+
+        dialoguebox = GameObject.Find("DialogueBox");
     }
 
-    public void StartDialogue (Dialogue dialogue)
+    public void StartDialogue ()
     {
-        //Debug.Log("Starting conversation with " + dialogue.name);
+        Debug.Log("Starting conversation with " + dialogueDatabase.ElementAt(dialogueIndex).Key);
 
-        nameText.text = dialogue.name;
-        sentences.Clear();
+        nameText.text = dialogueDatabase.ElementAt(dialogueIndex).Key;
 
-        foreach (string sentence in dialogue.sentences)
-        {
-            sentences.Enqueue(sentence);
-        }
-
-        DisplayNextSentence();
+        DisplayNextSentence(dialogueDatabase.ElementAt(dialogueIndex).Key);
     }
 
-    public void DisplayNextSentence() 
+    public void DisplayNextSentence(string speaker) 
     {
-        if (sentences.Count == 0)
+        if (dialogueIndex == 6)
         {
             EndDialogue();
             return;
         }
+        else if (dialogueIndex == 2){
+            dialoguebox.setActive(false);
+        }
 
-        string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
-        //Debug.Log(sentence);
+        dialogueText.text = dialogueDatabase[speaker];
+        dialogueDatabase[speaker];
+        dialogueIndex++;
     }
 
     void EndDialogue ()
--- Assets/scripts/DialogueTrigger.cs
+++ Assets/scripts/DialogueTrigger.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public Dialogue dialogue;
-
     public void TriggerDialogue ()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        FindObjectOfType<DialogueManager>().StartDialogue();
     }
 }
