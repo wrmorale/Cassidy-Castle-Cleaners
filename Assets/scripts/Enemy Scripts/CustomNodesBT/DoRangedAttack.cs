469065e6729e053a13f92f4e206fa5bc17638232
Wrmorales <wrmorale@ucsc.edu>
2023-04-28 23:29:01 -0700

    Moth BT operational

---
+++ Assets/scripts/Enemy Scripts/CustomNodesBT/DoRangedAttack.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+[System.Serializable]
+public class DoRangedAttack : ActionNode
+{
+    public string attackName = "";
+    AttackData attack;
+
+    /*Called the first time this node is ran on a tick.
+    If a node returns RUNNING it will not call OnStart()
+    on the next tick*/
+    protected override void OnStart() {
+        attack = context.attackManager.getAttack(attackName);
+        if (attack == null)
+            return;
+        Vector3 heading = (context.enemy.playerBody.position - context.rigidbody.position).normalized;    
+        Projectile clone = UnityEngine.Object.Instantiate(context.moth.projectilePrefab, context.moth.bulletSpawn.position, Quaternion.LookRotation(heading));
+        clone.gameObject.SetActive(true);
+        clone.Initialize(context.moth.projectileSpeed, context.moth.projectileLifetime, context.moth.projectileDamage, 1f, heading);
+        
+
+    }
+
+    /*Called after OnUpdate if the node returns SUCCESS or 
+    FAILURE. Also called if the current behavior using the
+    node function Abort()*/
+    protected override void OnStop() {
+    }
+
+    /*Called every tick that this node is executed*/
+    protected override State OnUpdate() {
+            return State.Success; 
+
+    }
+}
