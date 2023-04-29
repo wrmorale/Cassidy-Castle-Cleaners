bdb4231cb6a1df574f8db2fc7607c99938898db4
Wrmorales <wrmorale@ucsc.edu>
2023-04-29 15:56:03 -0700

    tutorial scene work

--- Assets/scripts/DialogueManager.cs
+++ Assets/scripts/DialogueManager.cs
@@ -16,25 +16,26 @@ public class DialogueManager : MonoBehaviour
 
     public static int dialogueIndex;
 
-    private Dictionary<string, string> dialogueDatabase = new Dictionary<string, string>()
+
+    private Dictionary<int, Tuple<string, string>> dialogueDatabase = new Dictionary<int, Tuple<string, string>>()
     {
         {
-            "Cassidy", "Listen, new hire. I know you want to jump right into things, but we should go ahead and teach you the basics. First, clean these dust piles. Look around if you don't see them right in front of you.\r\n"
+            0, Tuple.Create("Cassidy", "Listen, new hire. I know you want to jump right into things, but we should go ahead and teach you the basics. First, clean these dust piles. Look around if you don't see them right in front of you.")
         },
         {
-            "The Maid", "I'm not that new to cleaning, you know ..."
+            1, Tuple.Create("The Maid", "I'm not that new to cleaning, you know ...")
         },
         {
-            "Cassidy", "Good work. Make sure you move to the next room, and quickly."
+            2, Tuple.Create("Cassidy", "Good work. Make sure you move to the next room, and quickly.")
         },
         {
-            "Cassidy", "Oh, we didn't tell you but I hope you know how to fight, because this castle is full of monsters. Good luck."
+            3, Tuple.Create("Cassidy", "Oh, we didn't tell you but I hope you know how to fight, because this castle is full of monsters. Good luck.")
         },
         {
-            "The Maid", "Talk about a hostile work environment."
+            4, Tuple.Create("The Maid","Talk about a hostile work environment.")
         },
         {
-            "Cassidy", "Congradulation on not dying. Remember, you get paid to clean everything, not just the floors, so take out these monsters while you are at it."
+            5, Tuple.Create("Cassidy","Congratulations on not dying. Remember, you get paid to clean everything, not just the floors, so take out these monsters while you are at it.")
         }
 
     };
@@ -49,15 +50,22 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue ()
     {
-        Debug.Log("Starting conversation with " + dialogueDatabase.ElementAt(dialogueIndex).Key);
+        string speaker = (dialogueDatabase.ElementAt(dialogueIndex).Value).Item1;
+        Debug.Log("Starting conversation with " + speaker);
 
-        nameText.text = dialogueDatabase.ElementAt(dialogueIndex).Key;
+        nameText.text = speaker;
 
         DisplayNextSentence();
     }
 
     public void DisplayNextSentence() 
     {
+        string speaker = (dialogueDatabase.ElementAt(dialogueIndex).Value).Item1;
+        Debug.Log("Starting conversation with " + speaker);
+
+        nameText.text = speaker;
+
+        string speakerText = (dialogueDatabase.ElementAt(dialogueIndex).Value).Item2;
         if (dialogueIndex == 6)
         {
             EndDialogue();
@@ -66,9 +74,10 @@ public class DialogueManager : MonoBehaviour
         else if (dialogueIndex == 2){
             dialoguebox.SetActive(false);
         }
-
-        dialogueText.text = dialogueDatabase.ElementAt(dialogueIndex).Value;
-        dialogueIndex++;
+        else {
+             dialogueText.text = speakerText;
+             dialogueIndex++;
+        }
     }
 
     void EndDialogue ()
