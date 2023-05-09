cf155b60a6504c1452b79e94095cb4efb4162215
Santiago Ponce <saponce@ucsc.edu>
2023-05-08 22:19:53 -0700

    Bunny has a death anim

--- Assets/scripts/Enemy Scripts/Enemy.cs
+++ Assets/scripts/Enemy Scripts/Enemy.cs
@@ -114,7 +114,6 @@ public class Enemy : MonoBehaviour
             {
                 isDead = true; //Heavy is dead!
                 BTrunner.tree.rootNode.Abort();
-                GetComponentInChildren<Animator>().SetBool("Dead", true);
                 return;
             }
 
