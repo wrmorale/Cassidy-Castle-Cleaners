40f6f06ca9de2ef0789a2f944369c3639e7bbd7b
Wrmorales <wrmorale@ucsc.edu>
2023-05-24 18:58:16 -0700

    Added new pause menu to scenes

---
+++ Assets/scripts/UI Scripts/Custom Elements/PauseMenu.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused = false;
+    public PlayerInput playerInput;
+    public InputAction pauseAction;
+    public GameObject PauseMenuCanvas;
+    EventSystem eventSystem;
+    public GameObject resumeButton;
+    public GameObject controlsButton;
+    public GameObject controlButton;
+    public GameObject backButton;
+
+
+
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        pauseAction = playerInput.actions["Pause"];
+        eventSystem = EventSystem.current;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(pauseAction.triggered)
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+        
+        
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        PauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("Title_Scene");
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+
+        Application.Quit();
+    }
+
+    public void switchSelected()
+    {   
+        if(eventSystem.currentSelectedGameObject == controlsButton){
+            eventSystem.SetSelectedGameObject(controlButton);
+        }else if(eventSystem.currentSelectedGameObject == backButton){
+            eventSystem.SetSelectedGameObject(resumeButton);
+        }
+    }
+}
