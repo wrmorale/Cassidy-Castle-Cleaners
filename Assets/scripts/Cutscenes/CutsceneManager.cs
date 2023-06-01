30f4494928c8709a8320382081fc84b6538b6d07
Jessica Huang <jhuan221@ucsc.edu>
2023-06-01 12:46:03 -0700

    working cutscenes and transitions to credits once done

--- Assets/scripts/Cutscenes/CutsceneManager.cs
+++ Assets/scripts/Cutscenes/CutsceneManager.cs
@@ -5,49 +5,53 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CutsceneManager : MonoBehaviour
 {
     public GameObject continueButton;
     public static int cutsceneIndex;
     public bool roomCleared;
-    public Image[] cutsceneImages;
+    public Texture2D[] cutsceneImages;
     public Image displayImage;
+    public GameObject cutsceneCanvas;
+    public GameObject dialogueBox;
+    public GameObject portrait;
+
+    public float fadeDuration = 1.0f;
+    private CanvasGroup canvasGroup;
+
+    
 
     void Start()
     {
-        displayImage = GetComponent<Image>();
-    }
-
-    void Update()
-    {
-        if (roomCleared){
-            StartCutscene();
-        }
-    }
-
-    public void StartCutscene ()
-    {
+        displayImage = GameObject.Find("Cutscene").GetComponent<Image>();
         cutsceneIndex = 0;
-        
+        continueButton = GameObject.Find("ContinueButton");
+        dialogueBox = GameObject.Find("DialogueBox");
+        portrait = GameObject.Find("Portrait"); 
+        canvasGroup = GetComponent<CanvasGroup>();
         DisplayNextCutscene();
     }
 
     public void DisplayNextCutscene() 
     {
-        // set cutscene image 
-        displayImage = cutsceneImages[cutsceneIndex];
-
-        if(cutsceneIndex == 5){
+        if (cutsceneIndex == 5){
             EndCutscene();
         }
-        
+        else {
+            displayImage.sprite = Sprite.Create(cutsceneImages[cutsceneIndex], new Rect(0.0f, 0.0f, cutsceneImages[cutsceneIndex].width, cutsceneImages[cutsceneIndex].height), new Vector2(0.5f, 0.5f), 100.0f);
+        }
         cutsceneIndex++;
     }
 
-    void EndCutscene ()
+    public void EndCutscene()
     {   
         Debug.Log("End of cutscene.");
         cutsceneIndex = 0;
+        continueButton.SetActive(false);
+        // dialogueBox.SetActive(false);
+        // portrait.setActive(false);
+        SceneManager.LoadScene("Credits_Scene");
     }
 }
