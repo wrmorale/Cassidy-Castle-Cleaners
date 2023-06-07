383a8847f6df7243f20286052a2527a426565a46
vderbier <victor.derbier@gmail.com>
2023-06-07 14:53:27 -0700

    Turn off all props if blocking player

--- Assets/scripts/CamRays.cs
+++ Assets/scripts/CamRays.cs
@@ -7,34 +7,62 @@ public class CamRays : MonoBehaviour
     private ObjectFader fader;
 
     private GameObject lastHit = null;
+    private Renderer lastHitRenderer = null;
+    private GameObject player = null;
+    private CharacterController charController = null;
+
+    void Start()
+    {
+        player = GameObject.Find("maid");
+        charController =  GameObject.Find("Player").GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject player = GameObject.Find("maid");
 
         if (player != null)
         {
-            Vector3 dir = player.transform.position - transform.position;
-            Ray ray = new Ray(transform.position, dir);
+            // Ray cast to player
+            Vector3 offset = charController.center * player.transform.localScale.y;
+            Vector3 dir = player.transform.position + offset - transform.position;
+            Ray ray = new Ray(transform.position , dir);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider == null)
                     return;
+                
+                GameObject hitGameObject = hit.collider.gameObject;
 
-                if (hit.collider.gameObject == player || (lastHit != null && hit.collider.gameObject != lastHit))
+                if (hitGameObject == player || (lastHit != null && hitGameObject != lastHit))
                 {
                     lastHit = null;
                     if (fader != null)
                     {
                         fader.doFade = false;
                     }
+
+                    if (lastHitRenderer)
+                    {
+                        lastHitRenderer.enabled = true;
+                        lastHitRenderer = null;
+                    }
+                }
+                else if (hitGameObject.layer == 0 && hitGameObject.tag != "Player") // Default Layer
+                {
+                    Renderer currRenderer = hitGameObject.GetComponentInChildren<Renderer>();
+                    if (lastHitRenderer && lastHitRenderer != currRenderer)
+                        lastHitRenderer.enabled = true;
+                    lastHitRenderer = currRenderer;
+                    lastHitRenderer.enabled = false;
                 }
                 else
                 {
-                    lastHit = hit.collider.gameObject;
+                    if (lastHitRenderer)
+                        lastHitRenderer.enabled = true;
+                    lastHit = hitGameObject;
                     fader = lastHit.GetComponent<ObjectFader>();
                     if (fader != null)
                     {
