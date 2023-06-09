a238ff3f6147b40270be634788be2ccc378c1181
Andi Barron <andibarron7@gmail.com>
2023-06-09 00:55:37 -0700

    Revert "Revert "Merge branch 'andi-new-music'""
    This reverts commit aa99ba237befa81811957e3863a7d4a9375c6567.

--- Assets/scripts/MusicManager.cs
+++ Assets/scripts/MusicManager.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-
-    AudioSource audioSource;
+    public AudioSource audioSource;
     public AudioClip battleMusic;
+    public AudioClip menuMusic;
+    public AudioClip calmMusic;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
     }
 
-    private void playBattleMusic(){
+    public void playBattleMusic()
+    {
         audioSource.PlayOneShot(battleMusic, 1.0F);
     }
 
+    public void playMenuMusic()
+    {
+        audioSource.PlayOneShot(menuMusic, 3.0F);
+    }
+
+    public void playCalmMusic()
+    {
+        audioSource.PlayOneShot(calmMusic, 1.0F);
+    }
+    
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+
+    public bool IsMusicPlaying()
+    {
+        return audioSource.isPlaying;
+    }
 }
