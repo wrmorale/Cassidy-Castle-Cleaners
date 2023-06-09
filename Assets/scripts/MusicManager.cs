aa99ba237befa81811957e3863a7d4a9375c6567
Andi Barron <andibarron7@gmail.com>
2023-06-09 00:49:18 -0700

    Revert "Merge branch 'andi-new-music'"
    This reverts commit cb794ad19523cdcef922adc23d89900cf01223f9, reversing
    changes made to 60a175032f71efea4320cc102c370aeefda63fc3.

--- Assets/scripts/MusicManager.cs
+++ Assets/scripts/MusicManager.cs
@@ -4,39 +4,18 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    public AudioSource audioSource;
+
+    AudioSource audioSource;
     public AudioClip battleMusic;
-    public AudioClip menuMusic;
-    public AudioClip calmMusic;
 
     // Start is called before the first frame update
     void Start()
     {
-        //audioSource = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    public void playBattleMusic()
-    {
+    private void playBattleMusic(){
         audioSource.PlayOneShot(battleMusic, 1.0F);
     }
 
-    public void playMenuMusic()
-    {
-        audioSource.PlayOneShot(menuMusic, 3.0F);
-    }
-
-    public void playCalmMusic()
-    {
-        audioSource.PlayOneShot(calmMusic, 1.0F);
-    }
-    
-    public void StopMusic()
-    {
-        audioSource.Stop();
-    }
-
-    public bool IsMusicPlaying()
-    {
-        return audioSource.isPlaying;
-    }
 }
--- Assets/scripts/PersistentGameManager.cs
+++ Assets/scripts/PersistentGameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PersistentGameManager : MonoBehaviour
 {
@@ -9,7 +10,6 @@ public class PersistentGameManager : MonoBehaviour
 
     public Player player;
     public GameManager gameManager;
-    public MusicManager musicManager;
 
     // Game settings
     public bool infiniteManaCheat = false;
@@ -74,17 +74,6 @@ public class PersistentGameManager : MonoBehaviour
             mana = gameManager.mana;
         }
 
-        string currentSceneName = SceneManager.GetActiveScene().name;
-        if (currentSceneName == "Title_Scene") // Replace "MenuScene" with the actual name of your menu scene
-        {
-            musicManager.StopMusic();
-        }
-        else{
-            if (!musicManager.IsMusicPlaying()) // Check if music is not already playing
-            {
-                musicManager.playBattleMusic(); // Play the battle music
-            }
-        }
     }
 
     private void OnSceneChanged(Scene scene, LoadSceneMode mode)
